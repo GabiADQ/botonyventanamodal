9664344f12585ceffdaf93e9d7901eb4908007c8
GabiADQ <126377948+GabiADQ@users.noreply.github.com>
2023-03-12 08:03:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("miBoton").addEventListener('click', function () {
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Gracias por tu mensaje',
+            showConfirmButton: false,
+            timer: 1500
+        });
+    });
+    
